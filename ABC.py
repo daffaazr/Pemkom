86dbab6447f11c222c887ad1bb4380c7f1ac93b1
daffaazr <daffaazr@gmail.com>
2025-12-03 18:40:32 +0700

    Update ABC.py

--- ABC.py
+++ ABC.py
@@ -4,6 +4,22 @@ import numpy as np
 
 st.sidebar.title('KONTOL')
 page =  st.sidebar.selectbox('Pilih Halaman',['Crt','Crtt','Crrttt'])
+
+card = """
+<div style="
+    background-color:white;
+    padding:20px;
+    border-radius:18px;
+    box-shadow:0 4px 10px rgba(0,0,0,0.15);
+    margin-bottom:20px;
+">
+    <h3 style="color:#333;">📘 Informasi</h3>
+    <p style="font-size:16px;">Ini contoh card cantik di Streamlit.</p>
+</div>
+"""
+
+st.markdown(card, unsafe_allow_html=True)
+
 st.set_page_config()
 st.header('Pengujian Hipotesis')
 Tab1, Tab2, Tab3 = st.tabs(['ABC','BCD','DEF'])
