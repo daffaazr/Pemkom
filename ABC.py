d544c7edb1f2624e3748765ef2e0a19965100863
daffaazr <daffaazr@gmail.com>
2025-12-03 18:34:50 +0700

    Update ABC.py

--- ABC.py
+++ ABC.py
@@ -2,7 +2,8 @@ import streamlit as st
 import numpy as np
 
 
-
+st.sidebar.title('KONTOL')
+page =  st.sidebar.selectbox('Crt','Crtt','Crrttt')
 st.set_page_config()
 st.header('Pengujian Hipotesis')
 Tab1, Tab2, Tab3 = st.tabs(['ABC','BCD','DEF'])
