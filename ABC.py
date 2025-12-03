982b3eba564039b90ac1aa783755816c30655707
daffaazr <daffaazr@gmail.com>
2025-12-03 18:35:42 +0700

    Update ABC.py

--- ABC.py
+++ ABC.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 st.sidebar.title('KONTOL')
-page =  st.sidebar.selectbox('Crt','Crtt','Crrttt')
+page =  st.sidebar.selectbox(['Crt','Crtt','Crrttt'])
 st.set_page_config()
 st.header('Pengujian Hipotesis')
 Tab1, Tab2, Tab3 = st.tabs(['ABC','BCD','DEF'])
